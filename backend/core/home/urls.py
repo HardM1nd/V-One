f2f51b3f784492092ccc6f6ba72601a751d5358e
HardM1nd <iceonepm@gmail.com>
2025-12-06 17:13:45 +0300

    Add core folder

---
+++ backend/core/home/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from home.swagger import schema_view
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+]
