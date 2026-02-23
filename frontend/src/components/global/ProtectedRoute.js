7ca221b5483f800904f2f8b50437392d0ebcb34a
HardM1nd <iceonepm@gmail.com>
2026-02-24 00:00:33 +0300

    Clean

--- frontend/src/components/global/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import useUserContext from "../../contexts/UserContext";
-
-function LoginRequiredRoute() {
-    const { user } = useUserContext();
-    return <>{user ? <Outlet /> : <Navigate to="/signin"></Navigate>}</>;
-}
-
-export default LoginRequiredRoute;
